b6550d557729288f3f3da56279169782dd2324ae
B. Kevin Condor Romero (vaaroon) <vaaroon@outlook.com>
2024-10-16 19:39:05 +0200

    ✨ feat: Enhance TypeScript configuration and add custom color palette in Tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,24 @@ import animations from '@midudev/tailwind-animations'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary': '#26dce4',
+				'secondary': '#ddd',
+				'tertiary': '#000',
+				'primary-50': '#edfefd',
+				'primary-100': '#d1fcfc',
+				'primary-200': '#a9f7f8',
+				'primary-300': '#6eeff2',
+				'primary-400': '#26dce4',
+				'primary-500': '#0fc0cb',
+				'primary-600': '#109aaa',
+				'primary-700': '#147c8a',
+				'primary-800': '#196471',
+				'primary-900': '#1a525f',
+				'primary-950': '#0b3741',
+			},
+		},
 	},
 	plugins: [animations],
 }
