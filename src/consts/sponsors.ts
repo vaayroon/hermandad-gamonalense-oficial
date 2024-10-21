2b783b3227bfa1f69f16297d53eade08e5b573b2
B. Kevin Condor Romero (vaaroon) <vaaroon@outlook.com>
2024-10-21 18:35:24 +0200

    🍱 ✨ feat: Add Sponsors section and related types. update Layout and index pages. Footer has been moved to layout. Even though the sponsors section is created, it will not yet be displayed on the web.

---
+++ src/consts/sponsors.ts
@@ -0,0 +1,13 @@
+import type { Sponsors } from "@/types/Sponsors";
+
+export const SPONSORS: Array<Sponsors> = [
+	{
+		id: "huayucachi",
+		name: "Municipalidad Huayucachi",
+		url: "https://facebook.com/profile.php?id=100090310062404",
+		logo: {
+			width: 150,
+			height: 32,
+		},
+	},
+] as const
