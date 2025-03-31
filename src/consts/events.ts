12cf25177e3352d7342cffaea16d3f6fd3184a75
Bryan Kevin Condor Romero <vaayroon@outlook.com>
2025-03-31 08:38:03 +0200

    🚧 workin-on: Develop Cyclic slider gallery.

--- src/consts/events.ts
+++ src/consts/events.ts
@@ -7,10 +7,32 @@ export const EVENTS: Event[] = [
     title: "Festividad Tayta Niño 2025",
     date: "2025-01-26",
     category: EventCategories.Main,
-    description: "Celebración principal de la festividad Tayta Niño en Huayucachi",
+    description:
+      "Celebración principal de la festividad Tayta Niño en Huayucachi",
     prioste: "Juan Pérez Mamani",
-    imageUrl: "https://scontent-bcn1-1.xx.fbcdn.net/v/t39.30808-6/476562150_1189998699795570_3272211094281756444_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=-y36IcxT8PQQ7kNvgG2jhzx&_nc_oc=Adghx55cTbqsL6pGihgAkl7z_yIpDWn1m4nZnqBBtq2MKivytVc6pzoTJZK_V9M1e86Lfb8-uSnq37mwYUsVykte&_nc_zt=23&_nc_ht=scontent-bcn1-1.xx&_nc_gid=Y8oMlpXobtu8WNRr2SqoHw&oh=00_AYFheIWI7DP4-wN8_6vZtjb7pUEHOU4_wnq6jJuIBVT-tg&oe=67DCB349",
-    gallery: ["/images/gallery/event1-1.jpg", "/images/gallery/event1-2.jpg"]
+    imageUrl:
+      "https://scontent-bcn1-1.xx.fbcdn.net/v/t39.30808-6/476562150_1189998699795570_3272211094281756444_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=-y36IcxT8PQQ7kNvgG2jhzx&_nc_oc=Adghx55cTbqsL6pGihgAkl7z_yIpDWn1m4nZnqBBtq2MKivytVc6pzoTJZK_V9M1e86Lfb8-uSnq37mwYUsVykte&_nc_zt=23&_nc_ht=scontent-bcn1-1.xx&_nc_gid=Y8oMlpXobtu8WNRr2SqoHw&oh=00_AYFheIWI7DP4-wN8_6vZtjb7pUEHOU4_wnq6jJuIBVT-tg&oe=67DCB349",
+    gallery: [
+      "cover.webp",
+      "5.webp",
+      "AP3.webp",
+      "ARP1.webp",
+      "ARP2.webp",
+      "ARP4.webp",
+      "ARP5.webp",
+      "CP1.webp",
+      "CP2.webp",
+      "NW1.webp",
+      "NW2.webp",
+      "NW3.webp",
+      "NW4.webp",
+      "NW5.webp",
+      "NWL1.webp",
+      "NWL2.webp",
+      "NWL3.webp",
+      "NWL4.webp",
+      "NWL5.webp",
+    ],
   },
   {
     id: "0559e10d-27ee-4e92-ac3e-7e99eb946f30",
@@ -18,8 +40,9 @@ export const EVENTS: Event[] = [
     date: "2025-02-02",
     category: EventCategories.Institutional,
     description: "10° Aniversario de nuestra querida hermandad",
-    imageUrl: "https://scontent-bcn1-1.xx.fbcdn.net/v/t39.30808-6/476610185_1148125423980553_8319415945465157033_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_ohc=GE1a4shv80UQ7kNvgGdGNXJ&_nc_oc=Adgk96tONeUSIeJd6cGTEYCiYGzY5sNSJbNcSWeqDZpfs8BtOPwv63wZIRKRwfsTgnyoVyDPFBc907rNu0nNh-vV&_nc_zt=23&_nc_ht=scontent-bcn1-1.xx&_nc_gid=6ArgiCgb8k2kVXXx418UEQ&oh=00_AYELSEzSm-ZQw0EuqL_djZz75S19liVDFmbYCnNKDxYG5Q&oe=67DCA182",
-    gallery: ["/images/gallery/aniv-1.jpg"]
+    imageUrl:
+      "https://scontent-bcn1-1.xx.fbcdn.net/v/t39.30808-6/476610185_1148125423980553_8319415945465157033_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_ohc=GE1a4shv80UQ7kNvgGdGNXJ&_nc_oc=Adgk96tONeUSIeJd6cGTEYCiYGzY5sNSJbNcSWeqDZpfs8BtOPwv63wZIRKRwfsTgnyoVyDPFBc907rNu0nNh-vV&_nc_zt=23&_nc_ht=scontent-bcn1-1.xx&_nc_gid=6ArgiCgb8k2kVXXx418UEQ&oh=00_AYELSEzSm-ZQw0EuqL_djZz75S19liVDFmbYCnNKDxYG5Q&oe=67DCA182",
+    gallery: ["/images/gallery/aniv-1.jpg"],
   },
   {
     id: "48cd887b-963c-4702-9462-099e5a828718",
@@ -27,7 +50,9 @@ export const EVENTS: Event[] = [
     date: "2026-05-20",
     category: EventCategories.Social,
     description: "Matrimonio de nuestro hermando Juanito con Juanita",
-    imageUrl: "https://scontent-bcn1-1.xx.fbcdn.net/v/t39.30808-6/476641690_1148133170646445_4552216106177949502_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=sijtpR281KIQ7kNvgHXv5mt&_nc_oc=AdjmwmjOjzaOB-4lwVcJ9RER3ZYL6w3vYlFO1E-i1cHK3pmMEWqm2u3QoX0CgUTUoOpEU3OJmFnMHsk6eTakiKk1&_nc_zt=23&_nc_ht=scontent-bcn1-1.xx&_nc_gid=0887VXKvyzP1MnrGlgvlag&oh=00_AYFlNwZM3BVKr66I5qCc4yLxNmrEWObQm2voMSm563gErA&oe=67DC91D6",
-    gallery: ["/images/gallery/aniv-1.jpg"]
-  }
-]
+    imageUrl:
+      "https://scontent-bcn1-1.xx.fbcdn.net/v/t39.30808-6/476641690_1148133170646445_4552216106177949502_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_ohc=sijtpR281KIQ7kNvgHXv5mt&_nc_oc=AdjmwmjOjzaOB-4lwVcJ9RER3ZYL6w3vYlFO1E-i1cHK3pmMEWqm2u3QoX0CgUTUoOpEU3OJmFnMHsk6eTakiKk1&_nc_zt=23&_nc_ht=scontent-bcn1-1.xx&_nc_gid=0887VXKvyzP1MnrGlgvlag&oh=00_AYFlNwZM3BVKr66I5qCc4yLxNmrEWObQm2voMSm563gErA&oe=67DC91D6",
+    gallery: ["/images/gallery/aniv-1.jpg"],
+  },
+];
+/* Rellena esta variable con los nombres de las imagenes que etan dentro de la carpeta public/img/events/main/89560ece-b15a-42db-a9dc-bdd154b8841d */
